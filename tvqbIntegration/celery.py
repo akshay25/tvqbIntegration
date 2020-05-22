efbdc539a1ff4d92d81b80ac6cc1f2a97b7cf209
Akshay Bairagi <akshay.bairagi25@gmail.com>
2020-05-22 15:55:04 +0530

    setup django project and celery

---
+++ tvqbIntegration/celery.py
@@ -0,0 +1,13 @@
+# http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from celery import shared_task
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tvqbIntegration.settings')
+
+pfiles = ['tasks.test_task']
+
+app = Celery('tvqbIntegration')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(pfiles, force=True)
