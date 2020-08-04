44e6e0f5bab7793fc8a9504a6f61428117abf463
Kapil Diwakar <kdevileye@gmail.com>
2020-08-05 05:28:24 +0530

    parent 98ae1ec60c8c629621950bc36bc64bb7d8f88e66 author Kapil Diwakar <kdevileye@gmail.com> 1596585504 +0530 committer Kapil Diwakar <kdevileye@gmail.com> 1597917242 +0530
    Bill Fetching From TrackVia
    
    Bill and Invoive divison
    
    Typo Fixed
    
    Bill Fetching bug fixed
    
    Bill and Invoive divison
    
    Typo Fixed
    
    Pdf Download and Save support added
    
    Bill Support Added
    
    Untracker Files added
    
    Bill | Line Item Changes

---
+++ core/apis/quickBooks/vendor.py
@@ -0,0 +1,37 @@
+import requests
+import urllib.parse
+
+from django.conf import settings
+from core.apis.quickBooks.authentication import get_access_token, refresh
+
+
+def getVendor(name):
+    access_token = get_access_token()
+    sql = "select * from Vendor where DisplayName='{0}' and Active=true".format(name)
+    parsed_sql = urllib.parse.quote(sql)
+    url = '{0}/v3/company/{1}/query?query={2}&minorversion=52'.format(
+        settings.QBO_BASE_URL, settings.QBO_COMPANY_ID, parsed_sql)
+    headers = _get_headers(access_token)
+    response = requests.get(
+        url=url,
+        headers=headers
+    )
+    if response.status_code == 200:
+        result = response.json()
+        if result.get('QueryResponse'):
+            return {
+                'Vendor': result.get('QueryResponse').get('Vendor')[0]
+            }
+        else:
+            return {'error': 'NO Vendor FOUND'}
+    else:
+        print('Error: Vendor ', name)
+
+
+def _get_headers(access_token):
+    auth_header = 'Bearer {0}'.format(access_token)
+    headers = {
+        'Authorization': auth_header,
+        'Accept': 'application/json'
+    }
+    return headers
---
+++ core/billToExpenseMapper.py
@@ -0,0 +1,104 @@
+from core.apis.quickBooks.item import queryItem
+from core.apis.quickBooks.vendor import getVendor
+from core.email import send_email
+from core.logger import logger
+
+
+item_1_name = "Cost of Goods Sold:Purchases"
+item_2_name = "Freight"
+
+
+def billToExpense(bill_dict):
+    return _billMapper(bill_dict)
+
+
+def _billMapper(bill_dict):
+    return {
+        'TxnDate': bill_dict.get('BILL DATE'),
+        'DueDate': bill_dict.get('DUE DATE'),
+        'VendorRef': _getVendorRef(bill_dict.get('MANUFACTURER')),
+        'TotalAmt': bill_dict.get('BILL TOTAL'),
+        'Line': _getLineItems(
+            bill_dict.get('SUBTOTAL'), bill_dict.get('FREIGHT')),
+        # 'status': _getPaymentStatus(bill_dict.get('PAYMENT STATUS')),
+        'SyncToken': 1
+        # 'BillPDF': bill_dict.get('BILL PDF LINK')
+    }
+
+
+def _getVendorRef(name):
+    vendorRef = getVendor(name)
+    if not vendorRef:
+        logger.error(
+            'error finding customer: {0} in Quickbooks while processing trackvia bill'.format(name))
+        send_email('TV-QBO integeration error',
+                   'We got an error finding customer: {0} in Quickbooks while processing trackvia bill.'
+                   ' Bill creation/updation failed. Please create customer in quickbooks and retry.'.format(
+                       name))
+        # raise Exception()
+
+    return {
+        'name': vendorRef.get('DisplayName', 'Robertson & Associates') if vendorRef else 'Robertson & Associates',
+        'value': vendorRef.get('Id', '49') if vendorRef else '49',
+    }
+
+
+def _getPaymentStatus(key):
+    status_mapping_dict = {
+        'UNPAID': 'Pending'
+    }
+    return status_mapping_dict.get(key, 'Draft')  # Discus Logic
+
+
+def _getLineItems(subtotal, freight_charge):
+    lineList = []
+
+    item_1 = queryItem(item_1_name)
+    if item_1 and item_1.get('item'):
+        lineList.append(
+            {
+                # 'Id': '1',
+                # 'LineNum': '1',
+                'Description': "{0}/{1}".format(
+                    item_1.get('item').get('Type'),
+                    item_1.get('item').get('Description')),
+                'Amount': subtotal if subtotal else '0',
+                'DetailType': 'ItemBasedExpenseLineDetail',  # Logic to be discussed
+                'ItemBasedExpenseLineDetail': {
+                    'Qty': '1',  # item_1.get('item').get('#### '),
+                    'UnitPrice': item_1.get('item').get('Unit CN'),
+                    # 'Amount': item_1.get('item').get('Total CN'),
+                    'ItemRef': {
+                        'name': item_1.get('item').get('Name'),
+                        'value': item_1.get('item').get('Id')
+                    },
+                    'TaxCodeRef': {'value': 'TAX'}
+                }
+            }
+        )
+
+    item_2 = queryItem(item_2_name)
+    if item_2 and item_2.get('item'):
+        lineList.append(
+            {
+                # 'Id': '1',
+                # 'LineNum': '2',
+                'Description': "{0}/{1}".format(
+                    item_2.get('item').get('Type'),
+                    item_2.get('item').get('Description')),
+                'Amount': freight_charge if freight_charge else '0',
+                'DetailType': 'ItemBasedExpenseLineDetail',  # Logic to be discussed
+                'ItemBasedExpenseLineDetail': {
+                    'Qty': '1',  # item_1.get('item').get('#### '),
+                    'UnitPrice': item_2.get('item').get('Unit CN'),
+                    # 'Amount': item_2.get('item').get('Total CN'),
+                    'ItemRef': {
+                        'name': item_2.get('item').get('Name'),
+                        'value': item_2.get('item').get('Id')
+                    },
+                    'TaxCodeRef': {'value': 'TAX'}
+                }
+            }
+        )
+
+    return lineList
